8614ef2df92772c6429707ffbb16b0a5226880c8
larebfiroz <larebfiroz30208@gmail.com>
2024-07-21 20:35:19 +0530

    basic.js.animation

---
+++ condition.js
@@ -0,0 +1,19 @@
+n=56;
+if(
+    n%2===0
+)
+{ a='nice';
+  var x='some';
+    const y='my name';
+    console.log('even');
+   
+
+}
+else{
+    console.log('odd');
+}
+console.log(a);
+console.log(x);
+console.log(y);
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('hello javascript');
+console.log('hello nice');
+
+num=56;
+myname="lareb";
+console.log( typeof myname);
+console.log( typeof num);
+
+console.log(1 ==='1');
+console.log(1 =='1');
